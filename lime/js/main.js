3adbdf311d9b0a4a6e559d4b76471ac6eaaa6589
Yadong Wen <yadongwen@yadongwen-mbp.dhcp.thefacebook.com>
2019-02-27 17:47:25 -0800

    Prettify JS files with Prettier

--- lime/js/main.js
+++ lime/js/main.js
@@ -1,15 +1,12 @@
 if (!global._babelPolyfill) {
-  require('babel-polyfill')
+  require("babel-polyfill");
 }
 
+import Explanation from "./explanation.js";
+import Barchart from "./bar_chart.js";
+import PredictProba from "./predict_proba.js";
+import PredictedValue from "./predicted_value.js";
+require("../style.css");
 
-import Explanation from './explanation.js';
-import Barchart from './bar_chart.js';
-import PredictProba from './predict_proba.js';
-import PredictedValue from './predicted_value.js';
-require('../style.css');
-
-export {Explanation, Barchart, PredictProba, PredictedValue};
+export { Explanation, Barchart, PredictProba, PredictedValue };
 //require('style-loader');
-
-
